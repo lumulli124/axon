bf12a14db1e5830685bcc3110ecdf998960391ee
lumulli124 <151972218+lumulli124@users.noreply.github.com>
2026-04-03 09:31:30 -0600

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: 'https://github.com/lumulli124/axon/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
